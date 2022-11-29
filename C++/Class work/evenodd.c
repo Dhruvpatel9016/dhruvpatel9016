f6ead4884094b31fe8794cabea779d6f10b8c0bc
Dhruv patel <dhruvpatel98241@gmail.com>
2022-11-29 11:55:01 +0530

    29/11/22

---
+++ C++/Class work/evenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	printf("\nEnter A:- ");
+	scanf("%d",&a);
+	
+	if(a%2==0)
+	{
+		printf("\n%d is Even.",a);
+	}
+	else
+	{
+		printf("\n%d is Odd.",a);
+	}
+}
---
+++ C++/Class work/if_else.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	printf("\nEnter no. :- ");
+	scanf("%d",&a);
+	
+	if(a>0)
+	{
+		printf("\n It's +ve no.....'");
+	}
+	else
+	{
+		printf("\n It's -ve no.....'");
+	}
+}
